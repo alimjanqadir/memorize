638948ed99ad6f504c204c1f0048a5ed493dedff
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-04-05 16:44:01 +0800

    Lesson one finished.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0..<4){ index in
+                CardView(isFaceUp: false)
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 25.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 25.0)
+                    .stroke(lineWidth: 3)
+                    .foregroundColor(.orange)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 25.0).fill()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 Pro Max")
     }
 }
