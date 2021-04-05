3fcb4e23e51d4bd571f2c93fec6c83e7c0c7bf8f
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-04-05 15:37:03 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alimjan Qadir on 2021/4/5.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
